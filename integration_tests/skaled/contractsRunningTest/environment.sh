26a07f52c210d29d2452878cf7ddd022477a86dc
Dima Litvinov <dima.litvinov@skalelabs.com>
2020-08-13 20:34:01 +0300

    Start adding contractsRunningTest

---
+++ integration_tests/skaled/contractsRunningTest/environment.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set -x
+
+echo
+echo "----- integration_tests/skaled/contractsRunningTest/environment.sh ----- begin"
+
+export SKALED_CONTRACTS_TEST=$INTEGRATION_TESTS_DIR/skaled/contractsRunningTest
+echo "SKALED_CONTRACTS_TEST = $SKALED_CONTRACTS_TEST"
+
+export SKALE_EXPERIMANTAL=$SKALED_CONTRACTS_TEST/third_party/SkaleExperimental
+echo "SKALE_EXPERIMANTAL = $SKALE_EXPERIMANTAL"
+
+cd $SKALE_EXPERIMANTAL/l_sergiy/contractsRunningTest
+
+yarn install
+npx truffle compile
+
+echo "----- integration_tests/skaled/contractsRunningTest/environment.sh ----- end"
+echo
