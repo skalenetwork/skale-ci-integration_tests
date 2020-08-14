fa70dc4a7150048210c6d6f172581b2817efe782
Dima Litvinov <dima.litvinov@skalelabs.com>
2020-08-14 18:13:33 +0300

    Fix exports from skaled_providers

--- skaled_providers/endpoint_by_container/get_skaled.sh
+++ skaled_providers/endpoint_by_container/get_skaled.sh
@@ -22,14 +22,13 @@ echo -- Prepare config --
 python3 $SCRIPT_DIR/config.py merge $SCRIPT_DIR/config0.json ${@:1} >$SCRIPT_DIR/data_dir/config.json
 
 echo -- Get schain image --
-SKALED_RELEASE="1.46-develop.45"
 docker pull skalenetwork/schain:$SKALED_RELEASE
 
 echo -- Run container --
 docker run -d -v $SCRIPT_DIR/data_dir:/data_dir -e DATA_DIR=/data_dir -i -t --stop-timeout 40 skalenetwork/schain:$SKALED_RELEASE --http-port 1234 --config /data_dir/config.json -d /data_dir --ipcpath /data_dir -v 3 --web3-trace --enable-debug-behavior-apis --aa no
 
-ENDPOINT_URL="http://127.0.0.1:1234"
-CHAIN_ID=$( python3 $SCRIPT_DIR/config.py extract $SCRIPT_DIR/data_dir/config.json params.chainID )
-SCHAIN_OWNER=$( python3 $SCRIPT_DIR/config.py extract $SCRIPT_DIR/data_dir/config.json skaleConfig.sChain.schainOwner )
+export ENDPOINT_URL="http://127.0.0.1:1234"
+export CHAIN_ID=$( python3 $SCRIPT_DIR/config.py extract $SCRIPT_DIR/data_dir/config.json params.chainID )
+export SCHAIN_OWNER=$( python3 $SCRIPT_DIR/config.py extract $SCRIPT_DIR/data_dir/config.json skaleConfig.sChain.schainOwner )
 
 sleep 5
--- skaled_providers/endpoint_from_local_binary/get_skaled.sh
+++ skaled_providers/endpoint_from_local_binary/get_skaled.sh
@@ -24,8 +24,8 @@ python3 $SCRIPT_DIR/config.py merge $SCRIPT_DIR/config0.json ${@:1} >$SCRIPT_DIR
 echo -- Run binary --
 DATA_DIR=$SCRIPT_DIR/data_dir $SKTEST_EXE --http-port 1234 --config $SCRIPT_DIR/data_dir/config.json -d $SCRIPT_DIR/data_dir --ipcpath $SCRIPT_DIR/data_dir -v 2 --web3-trace --enable-debug-behavior-apis --aa no 2>$SCRIPT_DIR/data_dir/aleth.err >$SCRIPT_DIR/data_dir/aleth.out
 
-ENDPOINT_URL="http://127.0.0.1:1234"
-CHAIN_ID=$( python3 $SCRIPT_DIR/config.py extract $SCRIPT_DIR/data_dir/config.json params.chainID )
-SCHAIN_OWNER=$( python3 $SCRIPT_DIR/config.py extract $SCRIPT_DIR/data_dir/config.json skaleConfig.sChain.schainOwner )
+export ENDPOINT_URL="http://127.0.0.1:1234"
+export CHAIN_ID=$( python3 $SCRIPT_DIR/config.py extract $SCRIPT_DIR/data_dir/config.json params.chainID )
+export SCHAIN_OWNER=$( python3 $SCRIPT_DIR/config.py extract $SCRIPT_DIR/data_dir/config.json skaleConfig.sChain.schainOwner )
 
 sleep 5
