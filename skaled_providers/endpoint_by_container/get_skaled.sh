693545647a8cf6ba8618c1f6c53b7305e817da7e
Dima Litvinov <dima.litvinov@skalelabs.com>
2020-08-14 15:12:12 +0300

    Hide skaled logs and run $SKALED_PROVIDER in load_js

--- skaled_providers/endpoint_by_container/get_skaled.sh
+++ skaled_providers/endpoint_by_container/get_skaled.sh
@@ -15,7 +15,7 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 docker stop $(docker ps -a -q) && docker rm $(docker ps -a -q) || true
 
 echo -- Prepare data_dir --
-sudo rm -rf $SCRIPT_DIR/data_dir
+rm -rf $SCRIPT_DIR/data_dir
 mkdir $SCRIPT_DIR/data_dir
 
 echo -- Prepare config --
@@ -26,7 +26,7 @@ SKALED_RELEASE="1.46-develop.45"
 docker pull skalenetwork/schain:$SKALED_RELEASE
 
 echo -- Run container --
-docker run -d -v $SCRIPT_DIR/data_dir:/data_dir -e DATA_DIR=/data_dir -i -t --stop-timeout 40 skalenetwork/schain:$SKALED_RELEASE --http-port 1234 --config /data_dir/config.json -d /data_dir --ipcpath /data_dir -v 3 --web3-trace --enable-debug-behavior-apis --aa no&
+docker run -d -v $SCRIPT_DIR/data_dir:/data_dir -e DATA_DIR=/data_dir -i -t --stop-timeout 40 skalenetwork/schain:$SKALED_RELEASE --http-port 1234 --config /data_dir/config.json -d /data_dir --ipcpath /data_dir -v 3 --web3-trace --enable-debug-behavior-apis --aa no
 
 ENDPOINT_URL="http://127.0.0.1:1234"
 CHAIN_ID=$( python3 $SCRIPT_DIR/config.py extract $SCRIPT_DIR/data_dir/config.json params.chainID )
