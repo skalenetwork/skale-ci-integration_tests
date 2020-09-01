5269838a35e71dc57aff2ee420523c6ed3a370b2
Dima Litvinov <dima.litvinov@skalelabs.com>
2020-09-01 20:30:41 +0300

    Use ~/k2_aws.pem

--- skaled_providers/endpoint_by_terraform_with_containers/tf/destroy.sh
+++ skaled_providers/endpoint_by_terraform_with_containers/tf/destroy.sh
@@ -4,4 +4,4 @@ set -a
 . ./.env
 set +a
 
-cd tf_scripts && export TF_VAR_prefix=${DROPLET_NAME} && export TF_VAR_COUNT=${NODES} && terraform destroy -auto-approve
+cd tf_scripts && export TF_VAR_prefix=${DROPLET_NAME} && export TF_VAR_COUNT=${NODES} && terraform destroy -var="path_to_pem=~/k2_aws.pem" -auto-approve
