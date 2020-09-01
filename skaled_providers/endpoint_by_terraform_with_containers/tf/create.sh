53faecda5322f9fdc1e6e98fbf5758bb553a7821
Dima Litvinov <dima.litvinov@skalelabs.com>
2020-09-01 20:30:41 +0300

    Use ~/k2_aws.pem

--- skaled_providers/endpoint_by_terraform_with_containers/tf/create.sh
+++ skaled_providers/endpoint_by_terraform_with_containers/tf/create.sh
@@ -4,5 +4,5 @@ set -a
 . ./.env
 set +a
 
-cd tf_scripts && export TF_VAR_prefix=${DROPLET_NAME} && export TF_VAR_COUNT=${NODES} && terraform plan -out=tfplan && terraform apply -input=false tfplan
+cd tf_scripts && export TF_VAR_prefix=${DROPLET_NAME} && export TF_VAR_COUNT=${NODES} && terraform plan -out=tfplan && terraform apply -var="path_to_pem=~/k2_aws.pem" -input=false tfplan
 terraform output -json > ../output.json
