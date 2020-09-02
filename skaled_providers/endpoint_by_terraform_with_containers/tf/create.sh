d2c023e7def345224cf0f808c97609f85b664f1e
Dima Litvinov <dima.litvinov@skalelabs.com>
2020-09-02 20:23:02 +0300

    Use external vars

--- skaled_providers/endpoint_by_terraform_with_containers/tf/create.sh
+++ skaled_providers/endpoint_by_terraform_with_containers/tf/create.sh
@@ -4,5 +4,5 @@ set -a
 . ./.env
 set +a
 
-cd tf_scripts && export TF_VAR_prefix=${DROPLET_NAME} && export TF_VAR_COUNT=${NODES} && terraform plan -var="path_to_pem=~/elvis-oregon.pem" -out=tfplan && terraform apply -input=false tfplan
+cd tf_scripts && export TF_VAR_prefix=${DROPLET_NAME} && export TF_VAR_COUNT=${NUM_NODES} && terraform plan -var="path_to_pem=~/elvis-oregon.pem" -out=tfplan && terraform apply -input=false tfplan
 terraform output -json > ../output.json
