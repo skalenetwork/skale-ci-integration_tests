48d3411032073ff73fe110f8e79b5732d8896e16
Dima Litvinov <dima.litvinov@skalelabs.com>
2020-10-06 22:22:31 +0300

    Better timeout handling in skaled_to_chart.sh

--- integration_tests/skaled/load_js/skaled_to_chart.sh
+++ integration_tests/skaled/load_js/skaled_to_chart.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 ADDR=$1
 shopt -s lastpipe
+set -o pipefail
 
 while true
 do
-curl -X POST --data '{"jsonrpc":"2.0","method":"eth_blockNumber","params":[],"id":1}' $ADDR | jq '.result' | xargs printf '%d' | BN=$(</dev/stdin)
-curl -X POST --data '{"jsonrpc":"2.0","method":"debug_interfaceCall","params":["SkaleHost trace count drop_good"],"id":2}' $ADDR | jq '.result' | xargs echo | T1=$(</dev/stdin)
-curl -X POST --data '{"jsonrpc":"2.0","method":"debug_interfaceCall","params":["SkaleHost trace count import_consensus_born"],"id":3}' $ADDR | jq '.result' | xargs echo | T2=$(</dev/stdin)
+sleep 1
+
+curl -m 3 -X POST --data '{"jsonrpc":"2.0","method":"eth_blockNumber","params":[],"id":1}' $ADDR | jq '.result' | xargs printf '%d' | BN=$(</dev/stdin) || continue
+curl -m 3 -X POST --data '{"jsonrpc":"2.0","method":"debug_interfaceCall","params":["SkaleHost trace count drop_good"],"id":2}' $ADDR | jq '.result' | xargs echo | T1=$(</dev/stdin) || continue
+curl -m 3 -X POST --data '{"jsonrpc":"2.0","method":"debug_interfaceCall","params":["SkaleHost trace count import_consensus_born"],"id":3}' $ADDR | jq '.result' | xargs echo | T2=$(</dev/stdin) || continue
 
 TN=$(( $T1 + $T2 ))
 
 echo $( date +%s ) $BN $TN >>skaled_chart.txt
 
-sleep 1
 done
