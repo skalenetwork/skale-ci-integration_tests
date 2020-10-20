971ad0e8c6f90826670db05d11f1adabb82a2177
Dima Litvinov <dima.litvinov@skalelabs.com>
2020-10-21 00:53:43 +0300

    Fix typo

--- integration_tests/skaled/internals/test.sh
+++ integration_tests/skaled/internals/test.sh
@@ -25,7 +25,7 @@ case "$TEST_NAME" in
             echo "----- integration_tests/skaled/internals/test.sh::pytest -----"
 
             # to fix: test_race.py
-            pytest-3 --full-trace --showlocals -v -s test_chainid.py test_stop.py test_rotation.py
+            pytest --full-trace --showlocals -v -s test_chainid.py test_stop.py test_rotation.py
 
       ;;
 
