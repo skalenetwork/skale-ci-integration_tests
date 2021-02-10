91332456730a938a6a41793a61be3f367a2c8a68
Dima Litvinov <dima.litvinov@skalelabs.com>
2021-02-10 21:10:45 +0200

    Makde load_js long

--- integration_tests/skaled/load_js/skaled_to_chart.sh
+++ integration_tests/skaled/load_js/skaled_to_chart.sh
@@ -7,9 +7,9 @@ while true
 do
 sleep 1
 
-curl -m 3 -X POST --data '{"jsonrpc":"2.0","method":"eth_blockNumber","params":[],"id":1}' $ADDR | jq '.result' | xargs printf '%d' | BN=$(</dev/stdin) || continue
-curl -m 3 -X POST --data '{"jsonrpc":"2.0","method":"debug_interfaceCall","params":["SkaleHost trace count drop_good"],"id":2}' $ADDR | jq '.result' | xargs echo | T1=$(</dev/stdin) || continue
-curl -m 3 -X POST --data '{"jsonrpc":"2.0","method":"debug_interfaceCall","params":["SkaleHost trace count import_consensus_born"],"id":3}' $ADDR | jq '.result' | xargs echo | T2=$(</dev/stdin) || continue
+curl -m 6 -X POST --data '{"jsonrpc":"2.0","method":"eth_blockNumber","params":[],"id":1}' $ADDR | jq '.result' | xargs printf '%d' | BN=$(</dev/stdin) || continue
+curl -m 6 -X POST --data '{"jsonrpc":"2.0","method":"debug_interfaceCall","params":["SkaleHost trace count drop_good"],"id":2}' $ADDR | jq '.result' | xargs echo | T1=$(</dev/stdin) || continue
+curl -m 6 -X POST --data '{"jsonrpc":"2.0","method":"debug_interfaceCall","params":["SkaleHost trace count import_consensus_born"],"id":3}' $ADDR | jq '.result' | xargs echo | T2=$(</dev/stdin) || continue
 
 TN=$(( $T1 + $T2 ))
 
