a9c14211168ed8e242b91ac22a290d698ea58771
Dima Litvinov <dima.litvinov@skalelabs.com>
2021-03-17 18:30:24 +0200

    get_skaled seems to work

--- skaled_providers/endpoint_by_terraform_with_containers/get_skaled.sh
+++ skaled_providers/endpoint_by_terraform_with_containers/get_skaled.sh
@@ -10,9 +10,7 @@
 # CHAIN_ID - chainID
 # SCHAIN_OWNER - account with money and/or special permissions
 
-set -x
-
-NUM_NODES="${NUM_NODES:-2}"
+NUM_NODES="${NUM_NODES:-4}"
 SKALED_RELEASE="${SKALED_RELEASE:-develop-latest}"
 
 ORIG_CWD="$( pwd )"
@@ -21,10 +19,6 @@ cd "$SCRIPT_DIR"
 
 #SGX_URL="https://35.161.69.138:1026"
 SGX_URL="https://45.76.3.64:1026"
-if [ ! -f uniq.txt ]
-then
-    ./prepare_keys.sh $NUM_NODES $SGX_URL
-fi
 
 echo -- Free skaled --
 ./free_skaled.sh || true
@@ -45,92 +39,11 @@ for i in $( seq 0 $(($NUM_NODES-1)) )
 do
 	IPS[$i]=$( jq -r '.public_ips.value."skale-ci-'${i}'"' tf/output.json )
 	if [ "${IPS[$i]}" = "null" ]; then exit 1; fi
-done
-
-set +x
-
-echo -- Prepare config --
-
-echo '{ "skaleConfig": {"sChain": { "nodes": [' > _nodes.json
-
-I=0
-for IP in ${IPS[*]}
-do
-	
-	I=$((I+1))
-
-	read -r -d '' NODE_CFG <<- ****
-	{
-    	"nodeID": $I,
-    	"ip": "$IP",
-    	"basePort": 1231,
-    	"schainIndex" : $I,
-    	"publicKey":"0x$(echo $(jq '.result.publicKey' ecdsa$I.json) | xargs echo)",
-        "blsPublicKey0": $(jq '.BLSPublicKey["'$((I-1))'"]["0"]' keys$NUM_NODES.json),
-        "blsPublicKey1": $(jq '.BLSPublicKey["'$((I-1))'"]["1"]' keys$NUM_NODES.json),
-        "blsPublicKey2": $(jq '.BLSPublicKey["'$((I-1))'"]["2"]' keys$NUM_NODES.json),
-        "blsPublicKey3": $(jq '.BLSPublicKey["'$((I-1))'"]["3"]' keys$NUM_NODES.json)
-	}
-	****
-
-	echo "$NODE_CFG" >> _nodes.json
-
-	if [[ "$I" != "$NUM_NODES" ]]; then
-		echo "," >>_nodes.json
-	fi
-
-done
-
-echo "] } } }" >> _nodes.json
-
-python3 config.py merge config0.json ${@:1} _nodes.json >config.json
-rm _nodes.json
-
-I=0
-for IP in ${IPS[*]}
-do
-	
-	I=$((I+1))
-
-	read -r -d '' NODE_INFO <<- ****
-	{
-		"skaleConfig": {
-			"nodeInfo": {
-	      				"nodeName": "Node$I",
-	      				"nodeID": $I,
-	      				"bindIP": "0.0.0.0",
-	      				"basePort": 1231,
-	      				"ecdsaKeyName": $(jq '.result.keyName' ecdsa$I.json),
-	      				   "wallets": {
-                                "ima": {
-                                 "url": "$SGX_URL",
-                                 "keyShareName": "BLS_KEY:SCHAIN_ID:$(cat uniq.txt):NODE_ID:$I:DKG_ID:0",
-                                 "t": 3,
-                                 "n": 4,
-                                 "BLSPublicKey0": $(jq '.BLSPublicKey["'$((I-1))'"]["0"]' keys$NUM_NODES.json),
-                                 "BLSPublicKey1": $(jq '.BLSPublicKey["'$((I-1))'"]["1"]' keys$NUM_NODES.json),
-                                 "BLSPublicKey2": $(jq '.BLSPublicKey["'$((I-1))'"]["2"]' keys$NUM_NODES.json),
-                                 "BLSPublicKey3": $(jq '.BLSPublicKey["'$((I-1))'"]["3"]' keys$NUM_NODES.json),
-                                 "commonBLSPublicKey0": $(jq '.commonBLSPublicKey["0"]' keys$NUM_NODES.json),
-                                 "commonBLSPublicKey1": $(jq '.commonBLSPublicKey["1"]' keys$NUM_NODES.json),
-                                 "commonBLSPublicKey2": $(jq '.commonBLSPublicKey["2"]' keys$NUM_NODES.json),
-                                 "commonBLSPublicKey3": $(jq '.commonBLSPublicKey["3"]' keys$NUM_NODES.json)
-                                }
-                               }
-
-			}
-		}
-	}
-	****
-
-	echo "$NODE_INFO" > _node_info.json
-
-	python3 config.py merge config.json _node_info.json >config$I.json
+	LONG_IPS[$i]=${IPS[$i]}:1231
 done
 
 set -x
-
-rm _node_info.json
+SGX_URL="$SGX_URL" ./config_tools/make_configs.sh $NUM_NODES $(IFS=$','; echo "${LONG_IPS[*]}") $1
 
 echo -- Prepare nodes ---
 
