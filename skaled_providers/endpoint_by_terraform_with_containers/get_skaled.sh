41937ba9740bb038183b577bfa43b688020e31c3
Dima Litvinov <dima.litvinov@skalelabs.com>
2021-04-16 17:18:00 +0300

    Use monitoring scripts

--- skaled_providers/endpoint_by_terraform_with_containers/get_skaled.sh
+++ skaled_providers/endpoint_by_terraform_with_containers/get_skaled.sh
@@ -10,8 +10,8 @@
 # CHAIN_ID - chainID
 # SCHAIN_OWNER - account with money and/or special permissions
 
-NUM_NODES="${NUM_NODES:-4}"
-SKALED_RELEASE="${SKALED_RELEASE:-develop-latest}"
+export NUM_NODES="${NUM_NODES:-4}"
+export SKALED_RELEASE="${SKALED_RELEASE:-develop-latest}"
 
 ORIG_CWD="$( pwd )"
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
@@ -50,17 +50,18 @@ echo -- Prepare nodes ---
 I=0
 for IP in ${IPS[*]} #:0:11}
 do
-	
+
 	I=$((I+1))
 
 	scp -o "StrictHostKeyChecking no" config$I.json ubuntu@$IP:/home/ubuntu/config.json
 	scp -o "StrictHostKeyChecking no" filebeat.yml ubuntu@$IP:/home/ubuntu
 	scp -o "StrictHostKeyChecking no" create_btrfs.sh ubuntu@$IP:/home/ubuntu
 
+	scp -r -o "StrictHostKeyChecking no" skaled-debug/skaled_monitor ubuntu@$IP:/home/ubuntu
 	sudo scp -r -o "StrictHostKeyChecking no" /skale_node_data ubuntu@$IP:/home/ubuntu
-	
+
 	ssh -o "StrictHostKeyChecking no" ubuntu@$IP <<- ****
-	
+
 	curl -fsSL https://get.docker.com -o get-docker.sh
 	sudo sh get-docker.sh
 
@@ -69,27 +70,29 @@ do
 	sudo chown root:root filebeat.yml
 	sudo docker run -d --network host -u root -e FILEBEAT_HOST=3.17.12.121:5000 -v /home/ubuntu/filebeat.yml:/usr/share/filebeat/filebeat.yml:ro -v /var/lib/docker:/var/lib/docker:ro -v /var/run/docker.sock:/var/run/docker.sock docker.elastic.co/beats/filebeat:7.3.1
 
-	#sudo rm -rf data_dir	
-	#mkdir data_dir	
 	sudo BTRFS_DIR_PATH=data_dir ./create_btrfs.sh
 	sudo chown \$USER:\$USER data_dir
-	
+
 	sudo docker pull skalenetwork/schain:$SKALED_RELEASE
-	
-	for J in {0..0}
+
+	for J in {0..3}
 	do
 
 		#mv config.json data_dir/config.json
 		mkdir data_dir/\$J
-	
+
 		sed "s/1231,/1\$((2+J))31,/g" config.json > data_dir/\$J/config.json
 
-		sudo docker run -d -e catchupIntervalMs=60000 --cap-add SYS_ADMIN --name=skale-ci-$I-\$J -v /home/ubuntu/skale_node_data:/skale_node_data -v /home/ubuntu/data_dir/\$J:/data_dir -p 1\$((2+J))31-1\$((2+J))39:1\$((2+J))31-1\$((2+J))39/tcp -e DATA_DIR=/data_dir -i -t --stop-timeout 40 skalenetwork/schain:$SKALED_RELEASE --http-port 1\$((2+J))34 --config /data_dir/config.json -d /data_dir --ipcpath /data_dir -v 2 --web3-trace --enable-debug-behavior-apis --aa no
-	
-	done
-	
-	****
+		#sudo docker start skale-ci-$I-\$J
+		sudo docker run -d -e catchupIntervalMs=60000 --cap-add SYS_ADMIN --name=skale-ci-$I-\$J -v /home/ubuntu/skale_node_data:/skale_node_data -v /home/ubuntu/data_dir/\$J:/data_dir -p 1\$((2+J))31-1\$((2+J))39:1\$((2+J))31-1\$((2+J))39/tcp -e DATA_DIR=/data_dir -i -t --stop-timeout 40 skalenetwork/schain:$SKALED_RELEASE --http-port 1\$((2+J))34 --ws-port 1\$((2+J))33 --config /data_dir/config.json -d /data_dir --ipcpath /data_dir -v 2 --web3-trace --enable-debug-behavior-apis --aa no
 
+	done
+
+        cd skaled_monitor
+        sudo ./node-side-monitor.sh </dev/null 2>/dev/null >/dev/null &
+        cd ..
+
+	****
 done
 
 export ENDPOINT_URL="http://${IPS[0]}:1234"
