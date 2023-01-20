f29601a22377d5fcd285f01fd358e7c94cd41df2
Dima Litvinov <dima.litvinov@skalelabs.com>
2023-01-20 01:31:03 +0000

    Use 1g in blockchain-killer

--- integration_tests/skaled/stability/load_many_transactions.sh
+++ integration_tests/skaled/stability/load_many_transactions.sh
@@ -6,7 +6,7 @@ echo PRIVATE_KEY=0x72280f79fb45fa984fe2c5677141419cf3bf4fe239857a8f9ea7d302d75b2
 
 npx hardhat run scripts/deploy.ts --network custom
 
-npx hardhat setStorageUsage --size 1m --network custom&
+npx hardhat setStorageUsage --size 1g --network custom 2>&1 >blockchain-killer.log&
 
 cd ../..
 
