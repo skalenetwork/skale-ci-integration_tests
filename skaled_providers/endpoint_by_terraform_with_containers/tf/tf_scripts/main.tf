a4f903d823c3a2bdd0755ad5d22869d5b5d2e72d
Dima Litvinov <dima.litvinov@skalelabs.com>
2023-09-05 09:32:56 +0100

    Revert "Update main.tf add alias"
    This reverts commit 09a36ede59061a6cf119365d4a547495862718aa.

--- skaled_providers/endpoint_by_terraform_with_containers/tf/tf_scripts/main.tf
+++ skaled_providers/endpoint_by_terraform_with_containers/tf/tf_scripts/main.tf
@@ -1,11 +1,8 @@
-# https://cloud-images.ubuntu.com/locator/ec2/ for ami identication
-
 provider "aws" {
   access_key = var.access_key
   secret_key = var.secret_key
   region = var.region
 }
-
 
 provider "aws" {
   access_key = var.access_key
@@ -15,6 +12,7 @@ provider "aws" {
 }
 
 data "aws_ami" "ubuntu" {
+
   most_recent = true
 
   filter {
@@ -30,14 +28,13 @@ data "aws_ami" "ubuntu" {
   owners = ["099720109477"] # Canonical
 }
 
-
 resource "aws_volume_attachment" "ebs_att" {
   count = var.COUNT
   # skip_destroy = true
   device_name = "/dev/sdd"
-
+  
   volume_id   = aws_ebs_volume.lvm_volume[count.index].id
-  instance_id = var.spot_instance ? aws_spot_instance_request.node[count.index].spot_instance_id : aws_instance.node[count.index].id
+  instance_id = aws_spot_instance_request.node[count.index].spot_instance_id
 }
 
 resource "aws_ebs_volume" "lvm_volume" {
@@ -46,41 +43,24 @@ resource "aws_ebs_volume" "lvm_volume" {
   size = var.lvm_volume_size
 
   tags = {
-    Name = "${var.prefix}-${count.index}-lvm-volume"
-  }
+    Name = "${var.prefix}-${count.index}"
+  }  
 }
-
 
 resource "aws_spot_instance_request" "node" {
-  count = var.spot_instance ? var.COUNT : 0
-  spot_price    = var.spot_price[var.instance_type]
-  ami           = data.aws_ami.ubuntu.id
-  instance_type = var.instance_type
-  availability_zone = var.availability_zone
-  wait_for_fulfillment = true
-  # vpc_security_group_ids = [aws_security_group.security_group.id]
-  key_name = var.key_name
-
-  root_block_device {
-    volume_size = var.root_volume_size
-  }
-
-  tags = {
-    Name = "${var.prefix}-${count.index}"
-  }
-  # provisioner "local-exec" {
-  #   command = "echo 'node${count.index} ansible_host=${self.public_ip}' >> hosts"
-  # }
-}
-
-resource "aws_instance" "node" {
-  count = !var.spot_instance ? var.COUNT : 0
+  count = var.COUNT
   ami   = data.aws_ami.ubuntu.id
+  
   instance_type = var.instance_type
   availability_zone = var.availability_zone
+  # pem name
   key_name = var.key_name
-  # vpc_security_group_ids = [aws_security_group.security_group.id]
-
+  # Our Security group to allow HTTP and SSH access
+  # vpc_security_group_ids = [aws_security_group.default.id]
+  # for spot
+  spot_price    = var.spot_price[var.instance_type]
+  wait_for_fulfillment = true
+  #
   root_block_device {
     volume_size = var.root_volume_size
   }
@@ -88,9 +68,7 @@ resource "aws_instance" "node" {
   tags = {
     Name = "${var.prefix}-${count.index}"
   }
-  # provisioner "local-exec" {
-  #   command = "echo 'node${count.index} ansible_host=${self.public_ip}' >> hosts"
-  # }
+
   connection {
     type     = "ssh"
     user     = "ubuntu"
@@ -105,36 +83,12 @@ resource "aws_instance" "node" {
     source = "./scripts/authorized_keys"
     destination = "/home/ubuntu/.ssh/authorized_keys"
   }
-
 }
-
-
-data "aws_vpc" "default" {
-  default = true
-}
-
-
-resource "aws_eip_association" "eip_assoc" {
-  count = var.COUNT
-  allocation_id = aws_eip.node_eip[count.index].id
-  instance_id = var.spot_instance ? aws_spot_instance_request.node[count.index].spot_instance_id : aws_instance.node[count.index].id
-  provisioner "local-exec" {
-    command = "echo 'node${count.index} ansible_host=${self.public_ip}' >> hosts"
-  }
-}
-
-resource "aws_eip" "node_eip" {
-  count = var.COUNT
-}
-
 
 ##################### ALT ####################
 
-
-
-
-
 data "aws_ami" "ubuntu_alt" {
+
   provider = aws.alt
 
   most_recent = true
@@ -152,75 +106,56 @@ data "aws_ami" "ubuntu_alt" {
   owners = ["099720109477"] # Canonical
 }
 
-
 resource "aws_volume_attachment" "ebs_att_alt" {
-  count = var.COUNT
-  device_name = "/dev/sdd"
-
+  
   provider = aws.alt
 
+  count = var.COUNT
+  # skip_destroy = true
+  device_name = "/dev/sdd"
+  
   volume_id   = aws_ebs_volume.lvm_volume_alt[count.index].id
-  instance_id = var.spot_instance ? aws_spot_instance_request.node_alt[count.index].spot_instance_id : aws_instance.node_alt[count.index].id
+  instance_id = aws_spot_instance_request.node_alt[count.index].spot_instance_id
 }
 
 resource "aws_ebs_volume" "lvm_volume_alt" {
 
   provider = aws.alt
 
-
   count = var.COUNT
   availability_zone = var.availability_zone_alt
   size = var.lvm_volume_size
 
   tags = {
-    Name = "${var.prefix}-${count.index}-lvm-volume"
-  }
+    Name = "${var.prefix}-${count.index}"
+  }  
 }
-
 
 resource "aws_spot_instance_request" "node_alt" {
+
   provider = aws.alt
 
-  count = var.spot_instance ? var.COUNT : 0
-  ami           = data.aws_ami.ubuntu_alt.id
-
+  count = var.COUNT
+  ami   = data.aws_ami.ubuntu_alt.id
+  
   instance_type = var.instance_type
   availability_zone = var.availability_zone_alt
+  # pem name
+  key_name = var.key_name
+  # Our Security group to allow HTTP and SSH access
+  # vpc_security_group_ids = [aws_security_group.default.id]
+  # for spot
   spot_price    = var.spot_price[var.instance_type]
   wait_for_fulfillment = true
-  # vpc_security_group_ids = [aws_security_group.security_group.id]
-  key_name = var.key_name
-
+  #
   root_block_device {
     volume_size = var.root_volume_size
   }
 
   tags = {
-    Name = "${var.prefix}-${count.index}"
-  }
-  # provisioner "local-exec" {
-  #   command = "echo 'node${count.index} ansible_host=${self.public_ip}' >> hosts"
-  # }
-}
-
-resource "aws_instance" "node" {
-  count = !var.spot_instance ? var.COUNT : 0
-  ami   = data.aws_ami.ubuntu.id
-  instance_type = var.instance_type
-  availability_zone = var.availability_zone
-  key_name = var.key_name
-  # vpc_security_group_ids = [aws_security_group.security_group.id]
-
-  root_block_device {
-    volume_size = var.root_volume_size
+    Name = "${var.prefix}-${var.COUNT+count.index}"
   }
 
-  tags = {
-    Name = "${var.prefix}-${count.index}"
-  }
-  # provisioner "local-exec" {
-  #   command = "echo 'node${count.index} ansible_host=${self.public_ip}' >> hosts"
-  # }
   connection {
     type     = "ssh"
     user     = "ubuntu"
@@ -235,23 +170,15 @@ resource "aws_instance" "node" {
     source = "./scripts/authorized_keys"
     destination = "/home/ubuntu/.ssh/authorized_keys"
   }
-
 }
 
-data "aws_vpc" "default" {
-  default = true
-}
-
-
-resource "aws_eip_association" "eip_assoc_alt" {
-  count = var.COUNT
-  allocation_id = aws_eip.node_eip_alt[count.index].id
-  instance_id = var.spot_instance ? aws_spot_instance_request.node_alt[count.index].spot_instance_id : aws_instance.node_alt[count.index].id
-  provisioner "local-exec" {
-    command = "echo 'node${count.index} ansible_host=${self.public_ip}' >> hosts"
-  }
-}
-
-resource "aws_eip" "node_eip_alt" {
-  count = var.COUNT
+// outputs
+output "public_ips" {
+  description = "map output of the hostname and public ip for each instance"
+  value = zipmap(
+  # data.template_file.node_names.*.rendered,
+  concat(aws_spot_instance_request.node.*.tags.Name, aws_spot_instance_request.node_alt.*.tags.Name),
+  concat(aws_spot_instance_request.node.*.public_ip, aws_spot_instance_request.node_alt.*.public_ip)
+  #aws_eip.ip.*.public_ip
+  )
 }
